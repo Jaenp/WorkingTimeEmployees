9e7d381e61ed45e3c0b1cf883c30ccffe719bbcc
jpruiz052 <jpruiz052495@gmail.com>
2021-08-30 00:08:15 -0500

    Creation of record modification method for employee id.

--- WorkingTimeEmployees.Functions/Functions/WorkingTimeEmployeesAPI.cs
+++ WorkingTimeEmployees.Functions/Functions/WorkingTimeEmployeesAPI.cs
@@ -20,7 +20,6 @@ namespace WorkingTimeEmployees.Functions.Functions
         public static async Task<IActionResult> CreatedWorkingTimeEmployees(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "WorkingTimeEmployees")] HttpRequest req,
             [Table("WorkingTimeEmployees", Connection = "AzureWebJobsStorage")] CloudTable WorkingTimeEmployeesTable,
-
             ILogger log)
         {
             log.LogInformation("Recieved a new Working Time Employees");
@@ -58,7 +57,66 @@ namespace WorkingTimeEmployees.Functions.Functions
                 Message = "The information has been successfully registered"
             });
         }
-    
-    
+
+
+        [FunctionName(nameof(UpdatedWorkingTimeEmployees))]
+        public static async Task<IActionResult> UpdatedWorkingTimeEmployees(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "WorkingTimeEmployees/{Id_Employees}")] HttpRequest req,
+            [Table("WorkingTimeEmployees", Connection = "AzureWebJobsStorage")] CloudTable WorkingTimeEmployeesTable,
+            string Id_Employees,
+            ILogger log)
+        {
+            log.LogInformation($"The employee record will be updated: {Id_Employees}, in the table WorkingTimeEmployees");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            StructureTableWorkingTimeEmployees structureTableWorkingTimeEmployees = JsonConvert.DeserializeObject<StructureTableWorkingTimeEmployees>(requestBody);
+
+            TableOperation findOperationWorkingTimeEmployees = TableOperation.Retrieve<WorkingTimeEmployeesEntity>("WORKINGTIMEEMPLOYEES", Id_Employees);
+            TableResult findResultWorkingTimeEmployees = await WorkingTimeEmployeesTable.ExecuteAsync(findOperationWorkingTimeEmployees);
+
+            //Valid if the id employee was found successfully
+            if (findResultWorkingTimeEmployees.Result == null)
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    Message = $"The employee identified with the identification number: {Id_Employees}, was not found."
+                });
+            }
+
+            WorkingTimeEmployeesEntity workingTimeEmployeesEntity = (WorkingTimeEmployeesEntity)findResultWorkingTimeEmployees.Result;
+            workingTimeEmployeesEntity.RegistrationDateTime = structureTableWorkingTimeEmployees.RegistrationDateTime;
+            workingTimeEmployeesEntity.Type = structureTableWorkingTimeEmployees.Type;
+
+            //Validate the registration date and time
+            if (string.IsNullOrEmpty(structureTableWorkingTimeEmployees.RegistrationDateTime.ToString()))
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    Message = "The indicated request must comply with the following format in order to make the change, YYYY-MM-DD:HH:MM:SS",
+                });
+            }
+
+            // Validate Type
+            if (string.IsNullOrEmpty(structureTableWorkingTimeEmployees.Type.ToString()))
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    Message = "The data entered is not valid in the Type field."
+                });
+            }
+
+            
+            TableOperation updateOperationWorkingTimeEmployess = TableOperation.Replace(workingTimeEmployeesEntity);
+            await WorkingTimeEmployeesTable.ExecuteAsync(updateOperationWorkingTimeEmployess);
+
+            log.LogInformation("Employed update in the table Working Time Employed");
+
+            return new OkObjectResult(new Response
+            {
+                IdEmployees = workingTimeEmployeesEntity.Id_Employees,
+                Message = "The information has been successfully updated"
+            });
+        }
+
     }
 }
