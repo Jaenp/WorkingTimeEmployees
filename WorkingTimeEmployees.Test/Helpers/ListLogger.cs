f34be35dd08ef02525a82c9e4822d17ff1f1c4a2
jpruiz052 <jpruiz052495@gmail.com>
2021-09-06 00:41:46 -0500

    Updating the ListLogger class for unit tests

--- WorkingTimeEmployees.Test/Helpers/ListLogger.cs
+++ WorkingTimeEmployees.Test/Helpers/ListLogger.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace WorkingTimeEmployees.Test.Helpers
 {
-    class ListLogger
+    public class ListLogger : ILogger
     {
+        public ListLogger()
+        {
+            Logs = new List<string>();
+        }
+
+        public IList<string> Logs;
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return NullScope.Instance;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return false;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            string message = formatter(state, exception);
+            Logs.Add(message);
+        }
     }
 }
